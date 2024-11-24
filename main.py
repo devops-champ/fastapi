9d9e20fb189ceff84561af3b3e9779987223efee
Darshan Nagaraj <darshan-nag@pop-os.localdomain>
2024-11-24 22:36:54 +0530

    fastapi

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_user():
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_post():
+    return {"data": "This is your post"}
+
+
+@app.post("/createposts")
+def create_posts():
+    return {"message": "successfully created posts"}     
