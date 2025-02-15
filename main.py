eeb16dce6f976aa6f473ef4179929d9ec1d8d88f
devops-champ <dndarshan2@gmail.com>
2025-02-15 23:51:06 +0530

    adds get post path

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "Favorite Food", "content": "pizza is fav", "id": 2}]
 
 @app.get("/")
 def get_user():
-    return {"message": "Hello World"}
+    return {"message": "Hello Python"}
 
 @app.get("/posts")
 def get_post():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
-def create_posts():
-    return {"message": "successfully created posts"}     
+@app.post("/posts")
+def create_posts(post: Post):
+    print(post)
+    print(post.model_dump())
+    return {"data": "post"}
