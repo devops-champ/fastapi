3a0d02c1655fe1a2890ed64176235e6ca62c39fc
devops-champ <dndarshan2@gmail.com>
2025-02-19 22:20:03 +0530

    psycog CRUD implimentation

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from typing import Optional
 from fastapi import FastAPI, Body, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor #to display the columns
+import time
+
 
 app = FastAPI()
 
@@ -9,7 +13,20 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='1234', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connected")
+        break
+    except Exception as error:
+        print("connection failed")
+        print(error)
+        time.sleep(2)
+
+
 
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "Favorite Food", "content": "pizza is fav", "id": 2}]
@@ -31,20 +48,29 @@ def get_user():
 
 @app.get("/posts")
 def get_post():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    #print(posts)
+    return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.model_dump() #dict is depereated, so we need to use model_dump
-    post_dict['id'] = randrange(0, 100000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING * """,(post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+
+    # post_dict = post.model_dump() #dict is depereated, so we need to use model_dump
+    # post_dict['id'] = randrange(0, 100000)
+    # my_posts.append(post_dict)
+    return {"data": new_post}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
+    post = cursor.fetchone()
+    # post = find_post(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} was not found')
         # response.status_code = status.HTTP_404_NOT_FOUND
@@ -54,24 +80,30 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    delete = cursor.fetchone()
+    conn.commit()
+    # index = find_index_post(id)
 
-    if index == None:
+    if delete == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} does not exist')
 
-    my_posts.pop(index)
+    # my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
+    cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id),))
+    update = cursor.fetchone()
+    conn.commit()
+    # index = find_index_post(id)
 
-    if index == None:
+    if update == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} does not exist')
 
 
-    post_dict = post.model_dump()  #dict is depereated, so we need to use model_dump
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {'data': post_dict}    
+    # post_dict = post.model_dump()  #dict is depereated, so we need to use model_dump
+    # post_dict['id'] = id
+    # my_posts[index] = post_dict
+    return {'data': update }    
